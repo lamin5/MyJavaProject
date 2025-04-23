c327c760190ac9f2c0bfb1e6b60ede33b54d384d
lamin5 <say8913@gmail.com>
2025-04-23 14:18:54 +0900

    또 커밋?

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,12 @@
+/*
+ * javac HelloWorld.java
+ * java HelloWorld
+ * Run ¥‹√‡≈∞ : ctrl + f11
+ */
+
 
 public class HelloWorld {
-
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
 }
